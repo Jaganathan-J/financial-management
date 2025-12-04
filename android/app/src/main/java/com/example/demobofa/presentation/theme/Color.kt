80c4aece8dbac3061cf1fe32e56f576e05060f6a
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-12-04 15:25:02 -0500

    Auto commit generated Android file: app/src/main/java/com/example/demobofa/presentation/theme/Color.kt

---
+++ android/app/src/main/java/com/example/demobofa/presentation/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.demobofa.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BrandPrimary = Color(0xFF00438F) // Deep corporate Blue
+val BrandSecondary = Color(0xFFE31837) // Corporate Red
+val BrandBackground = Color(0xFFF2F2F2) // Light Gray
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF111111)
+val GrayText = Color(0xFF666666)
+val LightGray = Color(0xFFE0E0E0)
