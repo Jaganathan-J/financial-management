3145d358ce2d212ee40b495bd8e13adb6597b579
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-12-04 15:25:03 -0500

    Auto commit generated Android file: app/src/main/java/com/example/demobofa/presentation/theme/Type.kt

---
+++ android/app/src/main/java/com/example/demobofa/presentation/theme/Type.kt
@@ -0,0 +1,25 @@
+package com.example.demobofa.presentation.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    )
+)
